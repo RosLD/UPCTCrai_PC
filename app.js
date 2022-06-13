45737c38ed22429a297cb2cb10fe2dde1e9434f5
Daniel <griefros@gmail.com>
2022-06-13 19:38:50 +0200

    MQTT(EMQX) included

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-const { count } = require('console');
-const { Interface } = require('readline');
-const { SerialPort, ReadlineParser,ByteLengthParser } = require('serialport')
+const { SerialPort } = require('serialport')
 const Database = require("better-sqlite3");
+const mqtt = require("mqtt")
 
+/*MQTT*/
+const options = {
+    clean: true, // retain session
+connectTimeout: 4000, // Timeout period
+// Authentication information
+clientId: 'raspCRAI',
+username: 'raspCRAI',
+password: 'raspCRAI2022',
+}
+
+// Connect string, and specify the connection method by the protocol
+// ws Unencrypted WebSocket connection
+// wss Encrypted WebSocket connection
+// mqtt Unencrypted TCP connection
+// mqtts Encrypted TCP connection
+// wxs WeChat applet connection
+// alis Alipay applet connection
+const connectUrl = 'ws://localhost:8083/mqtt'
+const client = mqtt.connect(connectUrl, options)
+
+client.on('connect', function () {
+        console.log("Connected to MQTT URL")
+  })
+
+/*SQLITE3 - Local storagement*/
 const db = new Database("PersonCount.db")
 const createTable = 
     "CREATE TABLE IF NOT EXISTS PersonCounter ('Timestamp','IdSensor','CuentaActualSensor','CuentaGlobal')";
@@ -57,11 +81,13 @@ let chain1 = ''
 let subchain1 = ''
 let countchain1 = ''
 let cuenta1 = 0
+let param1 = {}
 
 let chain2 = ''
 let subchain2 = ''
 let countchain2 = ''
 let cuenta2 = 0
+let param2 = {}
 
 console.log("DEMO CONTADOR PERSONAS BIBLIOTECA ANTIGONES")
 
@@ -87,6 +113,11 @@ serialport1.on('data',function(buff){
             console.log(`Total de personas en la Biblioteca: ${total} - ${horaactual}`)
             insertInto.run(horaactual,"Left",cuenta1,total)
             console.log("------------------------------------------------------------------------------")
+            param1.Time = horaactual
+            param1.Sensor = "Left"
+            param1.SensorCount = cuenta1
+            param1.Total = total
+            client.publish("CRAIUPCTPersonCount",JSON.stringify(param1))
 
         }
     }
@@ -116,6 +147,11 @@ serialport2.on('data',function(buff){
             console.log(`Total de personas en la Biblioteca: ${total} - ${horaactual}`)
             insertInto.run(horaactual,"Right",cuenta2,total)
             console.log("------------------------------------------------------------------------------")
+            param2.Time = horaactual
+            param2.Sensor = "Left"
+            param2.SensorCount = cuenta2
+            param2.Total = total
+            client.publish("CRAIUPCTPersonCount",JSON.stringify(param2))
             
         }
        
@@ -123,3 +159,9 @@ serialport2.on('data',function(buff){
     }
         
 });
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
+    client.end()
+  })
